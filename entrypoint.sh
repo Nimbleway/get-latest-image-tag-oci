a6aacb1a67c709bebbc73ed01605f33727ac55d8
Eyal Halfon <eyalh@nimbleway.com>
2024-07-22 16:17:24 +0300

    fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
  
 IMAGE=$(oci artifacts container image list --compartment-id $COMPARTMENT_ID --repository-name $REPO_NAME --sort-by TIMECREATED --sort-order DESC | jq -r '.data.items[]."display-name"' | grep -v -E ':latest$|:chrome$' | head -n 1)
+echo $IMAGE
+echo "image=$(echo $IMAGE)" >> $GITHUB_OUTPUT
